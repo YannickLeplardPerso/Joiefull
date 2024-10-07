303be88160827f99e8d195da6249fe5b762325be
Yannick Leplard <leplard.yannick@gmail.com>
2024-10-07 11:04:46 +0200

    Initial Commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Yannick LEPLARD on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
